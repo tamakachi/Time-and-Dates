5c18169ddb8b4f0e756785522a595f48154198b0
Kent <kenttechnical@gmail.com>
2022-07-27 12:24:16 +0200

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+const now = new Date()
+
+console.log(now)
+
+// year, months, day, times
+
+console.log('getFullYear:',now.getFullYear())
+console.log('getDate:',now.getDate())
+
+console.log('getHours:',now.getHours())
+console.log('getMinutes:',now.getMinutes())
+console.log('getSeconds:',now.getSeconds())
+
+// Problematic function, returns value of month as a number -1
+// More bs from the array type nature of Javascript
+// Careful when implementing these into your code
+console.log('getMonth', now.getMonth()) 
+
+
+// Problematic function, returns value of day of the week as number-1
+
+console.log('getDay:',now.getDay())
